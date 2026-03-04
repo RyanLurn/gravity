23f89d1511eb8213ad90171bf0cacb3928485dad
Ryan Luong <lumintriet@gmail.com>
2026-03-04 10:50:53 +0700

    feat(cli): handle fallback error in the validate package manager util in case the package-manager-detector library throws.

--- apps/cli/src/helpers/validate-package-manager.ts
+++ apps/cli/src/helpers/validate-package-manager.ts
@@ -1,5 +1,6 @@
 import type { Failure, Success, Result } from "@gravity/types";
 
+import { createFallbackError, type FallbackError } from "@gravity/error";
 import { detect } from "package-manager-detector/detect";
 
 import {
@@ -8,31 +9,47 @@ import {
 } from "@/errors";
 
 export async function validatePackageManager(): Promise<
-  Result<"bun", UnsupportedPackageManagerError | PackageManagerDetectionError>
+  Result<
+    "bun",
+    | UnsupportedPackageManagerError
+    | PackageManagerDetectionError
+    | FallbackError
+  >
 > {
-  const detectResult = await detect();
-  if (!detectResult) {
-    const failure: Failure<PackageManagerDetectionError> = {
-      error: new PackageManagerDetectionError(),
+  try {
+    const detectResult = await detect();
+    if (!detectResult) {
+      const failure: Failure<PackageManagerDetectionError> = {
+        error: new PackageManagerDetectionError(),
+        success: false,
+      };
+
+      return failure;
+    }
+
+    if (detectResult.name !== "bun") {
+      const failure: Failure<UnsupportedPackageManagerError> = {
+        error: new UnsupportedPackageManagerError(),
+        success: false,
+      };
+
+      return failure;
+    }
+
+    const success: Success<"bun"> = {
+      success: true,
+      data: "bun",
+    };
+
+    return success;
+  } catch (error) {
+    const failure: Failure<FallbackError> = {
+      error: createFallbackError("Failed to detect package manager.", {
+        cause: error,
+      }),
       success: false,
     };
 
     return failure;
   }
-
-  if (detectResult.name !== "bun") {
-    const failure: Failure<UnsupportedPackageManagerError> = {
-      error: new UnsupportedPackageManagerError(),
-      success: false,
-    };
-
-    return failure;
-  }
-
-  const success: Success<"bun"> = {
-    success: true,
-    data: "bun",
-  };
-
-  return success;
 }
