ab6cb29750212643a662d3dd203ba70ea334132c
Ryan Luong <lumintriet@gmail.com>
2026-03-04 10:35:18 +0700

    feat(error): define build entry and bundle package.

---
+++ packages/error/src/index.ts
@@ -0,0 +1,5 @@
+export * from "@/utils/create-fallback";
+export * from "@/classes/unexpected";
+export * from "@/classes/unknown";
+export * from "@/utils/serialize";
+export * from "@/classes/custom";
